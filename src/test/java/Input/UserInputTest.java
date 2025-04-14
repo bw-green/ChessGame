d136d236e2fefab3aca0f1382b3c8b29a57fece2
bw-green <102360569+bw-green@users.noreply.github.com>
2025-04-15 02:34:42 +0900

    UserInput Test 케이스 추가 UserInput return 값 변경

---
+++ src/test/java/Input/UserInputTest.java
@@ -0,0 +1,39 @@
+package test.java.Input;
+
+import Input.GameInput;
+import data.GameInputReturn;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import userinput.UserInput;
+
+import java.io.ByteArrayInputStream;
+
+public class UserInputTest {
+
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "e2 f2",
+            "b1 c3  ",
+            "e2 e4",
+            "e1 e5                  "
+    })
+    @DisplayName("savefile명령어")
+    void InputTrueTest(String input) {
+        Assertions.assertEquals(GameInputReturn.COORDINATE_TRUE.getCode(), UserInput.handleInput(input));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "e 3 f2",
+    })
+    @DisplayName("savefile명령어")
+    void InputFalseTest(String input) {
+        Assertions.assertEquals(GameInputReturn.ERROR.getCode(), UserInput.handleInput(input));
+    }
+
+}
